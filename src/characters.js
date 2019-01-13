b67c982b6050bad908ef85103864e20299f570fb
Ted Weiss <theodore_weiss@outlook.com>
2019-01-12 23:18:29 -0500

    add characters file

---
+++ src/characters.js
@@ -0,0 +1,142 @@
+export const characters = [
+  {
+    name: 'invisible-woman',
+    side: 'hero',
+    colors: {
+      backgroundColor: '#d1516d',
+      header: '#333f42',
+      tabs: '#1e5599',
+      activeTabBottomColor: '#f2ead1'
+    },
+    terminalTextColors: {
+      black: '#000000',
+      red: '#ED0C05',
+      green: '#34E60F',
+      yellow: '#E5D30F',
+      blue: '#0A29CC',
+      magenta: '#CD0DE8',
+      cyan: '#07CCD9',
+      white: '#FFFFFF',
+      lightBlack: '#000000',
+      lightRed: '#FF0D06',
+      lightGreen: '#3AFF11',
+      lightYellow: '#FFEB10',
+      lightBlue: '#0C33FF',
+      lightMagenta: '#E10EFF',
+      lightCyan: '#08F0FF',
+      lightWhite: '#FFFFFF'
+    }
+  },
+  {
+    name: 'wasp',
+    side: 'hero',
+    colors: {
+      backgroundColor: '#c6bb54',
+      header: '#333f42',
+      tabs: '#817537',
+      activeTabBottomColor: '#42b9df'
+    },
+    terminalTextColors: {
+      black: '#000000',
+      red: '#ED0C05',
+      green: '#34E60F',
+      yellow: '#E5D30F',
+      blue: '#0A29CC',
+      magenta: '#CD0DE8',
+      cyan: '#07CCD9',
+      white: '#FFFFFF',
+      lightBlack: '#000000',
+      lightRed: '#FF0D06',
+      lightGreen: '#3AFF11',
+      lightYellow: '#FFEB10',
+      lightBlue: '#0C33FF',
+      lightMagenta: '#E10EFF',
+      lightCyan: '#08F0FF',
+      lightWhite: '#FFFFFF'
+    }
+  },
+  {
+    name: 'x-23',
+    side: 'hero',
+    colors: {
+      backgroundColor: '#7d929d',
+      header: '#333f42',
+      tabs: '#38464e',
+      activeTabBottomColor: '#9b8463'
+    },
+    terminalTextColors: {
+      black: '#000000',
+      red: '#ED0C05',
+      green: '#34E60F',
+      yellow: '#E5D30F',
+      blue: '#0A29CC',
+      magenta: '#CD0DE8',
+      cyan: '#07CCD9',
+      white: '#FFFFFF',
+      lightBlack: '#000000',
+      lightRed: '#FF0D06',
+      lightGreen: '#3AFF11',
+      lightYellow: '#FFEB10',
+      lightBlue: '#0C33FF',
+      lightMagenta: '#E10EFF',
+      lightCyan: '#08F0FF',
+      lightWhite: '#FFFFFF'
+    }
+  },
+  {
+    name: 'mystique',
+    side: 'villian',
+    colors: {
+      backgroundColor: '#a1ada7',
+      header: '#333f42',
+      tabs: '#125c9d',
+      activeTabBottomColor: '#6c221c'
+    },
+    terminalTextColors: {
+      black: '#000000',
+      red: '#ED0C05',
+      green: '#34E60F',
+      yellow: '#E5D30F',
+      blue: '#0A29CC',
+      magenta: '#CD0DE8',
+      cyan: '#07CCD9',
+      white: '#FFFFFF',
+      lightBlack: '#000000',
+      lightRed: '#FF0D06',
+      lightGreen: '#3AFF11',
+      lightYellow: '#FFEB10',
+      lightBlue: '#0C33FF',
+      lightMagenta: '#E10EFF',
+      lightCyan: '#08F0FF',
+      lightWhite: '#FFFFFF'
+    }
+  },
+  {
+    name: 'phoenix',
+    side: 'villian',
+    colors: {
+      backgroundColor: '#f8d082',
+      header: '#333f42',
+      tabs: '#7c3129',
+      activeTabBottomColor: '#ed9646'
+    },
+    terminalTextColors: {
+      black: '#000000',
+      red: '#ED0C05',
+      green: '#34E60F',
+      yellow: '#E5D30F',
+      blue: '#0A29CC',
+      magenta: '#CD0DE8',
+      cyan: '#07CCD9',
+      white: '#FFFFFF',
+      lightBlack: '#000000',
+      lightRed: '#FF0D06',
+      lightGreen: '#3AFF11',
+      lightYellow: '#FFEB10',
+      lightBlue: '#0C33FF',
+      lightMagenta: '#E10EFF',
+      lightCyan: '#08F0FF',
+      lightWhite: '#FFFFFF'
+    }
+  }
+]
