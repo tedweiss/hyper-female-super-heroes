5293c1a7361ea35c60b84142ef144ce93937c6ff
Ted Weiss <theodore_weiss@outlook.com>
2019-01-12 23:20:06 -0500

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  node: {
+    __dirname: false
+  }
+}
