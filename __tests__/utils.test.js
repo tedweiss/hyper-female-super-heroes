eac66eb0ef7e57e5a69910720d4e9bd7fd8043ef
Ted Weiss <theodore_weiss@outlook.com>
2019-01-15 00:22:22 -0500

    test should return randomly selected character from user's list if more than one is entered into the array

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -12,4 +12,9 @@ describe('getCharacter', () => {
     let invisibleWoman = characters[0]
     expect(getCharacter(userSelectedCharacters)).toEqual(invisibleWoman)
   })
+  test("should return randomly selected character from user's list if more than one is entered into the array", () => {
+    let userSelectedCharacters = ['invisible-woman', 'x-23', 'wasp']
+    let chosenName = getCharacter(userSelectedCharacters).name
+    expect(userSelectedCharacters).toEqual(expect.arrayContaining([chosenName]))
+  })
 })
