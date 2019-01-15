161683fa3a252380a2df3febcffb65af83685e4a
Ted Weiss <theodore_weiss@outlook.com>
2019-01-14 23:31:10 -0500

    added test for getCharacter. test should return user selected character if only one is input into the array

---
+++ __tests__/utils.test.js
@@ -0,0 +1,10 @@
+import { characters } from '../src/characters'
+import { getCharacter } from '../src/utils'
+
+describe('getCharacter', () => {
+  test('should return user selected character if only one is input into the array', () => {
+    let userSelectedCharacters = ['invisible-woman']
+    let invisibleWoman = characters[0]
+    expect(getCharacter(userSelectedCharacters)).toEqual(invisibleWoman)
+  })
+})
