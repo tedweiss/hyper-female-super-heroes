9bbfd6840dc83c6ea8fb16e1114a8a90f7e91046
Ted Weiss <theodore_weiss@outlook.com>
2019-01-14 23:34:13 -0500

    test should return user selected character if only one is input as a string

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -7,4 +7,9 @@ describe('getCharacter', () => {
     let invisibleWoman = characters[0]
     expect(getCharacter(userSelectedCharacters)).toEqual(invisibleWoman)
   })
+  test('should return user selected character if only one is input as a string', () => {
+    let userSelectedCharacters = 'invisible-woman'
+    let invisibleWoman = characters[0]
+    expect(getCharacter(userSelectedCharacters)).toEqual(invisibleWoman)
+  })
 })
