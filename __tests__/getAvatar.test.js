a082664684c8f01acb46ae5d448e6fe83c7ace07
Ted Weiss <theodore_weiss@outlook.com>
2019-01-23 21:00:26 -0500

    test should return true when user selects 'true' for wanting an avatar

--- __tests__/getAvatar.test.js
+++ __tests__/getAvatar.test.js
@@ -4,4 +4,7 @@ describe('getAvatar', () => {
   test('should return true when user selects true for wanting an avatar', () => {
     expect(getAvatar(true)).toEqual(true)
   })
+  test("should return true when user selects 'true' for wanting an avatar", () => {
+    expect(getAvatar('true')).toEqual(true)
+  })
 })
