dde1d3a00d8a2e17821f61b307f7abb02283b173
Ted Weiss <theodore_weiss@outlook.com>
2019-01-23 21:09:10 -0500

    test should return false when user selects 'false' for not wanting an avatar

--- __tests__/getAvatar.test.js
+++ __tests__/getAvatar.test.js
@@ -13,4 +13,7 @@ describe('getAvatar', () => {
   test('should return false when user selects false for not wanting an avatar', () => {
     expect(getAvatar(false)).toEqual(false)
   })
+  test("should return false when user selects 'false' for not wanting an avatar", () => {
+    expect(getAvatar('false')).toEqual(false)
+  })
 })
