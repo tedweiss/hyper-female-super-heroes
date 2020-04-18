3145f57ab54b76dda6cda1da0c0840e104a2832c
Hilary Claire <hilary.wilson2563@gmail.com>
2020-04-17 20:15:54 -0400

    add new characters and colors

--- src/characters.js
+++ src/characters.js
@@ -1,272 +1,324 @@
-export const characters = [
-  {
-    name: 'invisible-woman',
-    side: 'heroes',
-    colors: {
-      backgroundColor: '#D1516D',
-      tabs: '#1E5599',
-      activeTabBottomColor: '#F2EAD1'
+export const characters = [{
+        name: 'bat-girl',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#5d5c5c',
+            tabs: '#3d4a8b',
+            activeTabBottomColor: '#e89d26'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#f24141',
+            green: '#aef241',
+            yellow: '#e89d26',
+            blue: '#4170f2',
+            magenta: '#C3A3FA',
+            cyan: '#07CCD9',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FFB5BE',
+            lightGreen: '#aef241',
+            lightYellow: '#efd384',
+            lightBlue: '#4170f2',
+            lightMagenta: '#CDBCFF',
+            lightCyan: '#08F0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#FAA5BA',
-      green: '#34E60F',
-      yellow: '#E5D30F',
-      blue: '#0A29CC',
-      magenta: '#C3A3FA',
-      cyan: '#07CCD9',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#FFB5BE',
-      lightGreen: '#3AFF11',
-      lightYellow: '#FFEB10',
-      lightBlue: '#0C33FF',
-      lightMagenta: '#CDBCFF',
-      lightCyan: '#08F0FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'ms-marvel',
-    side: 'heroes',
-    colors: {
-      backgroundColor: '#A58A49',
-      tabs: '#2778AB',
-      activeTabBottomColor: '#A9443D',
+    {
+        name: 'invisible-woman',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#D1516D',
+            tabs: '#1E5599',
+            activeTabBottomColor: '#F2EAD1'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#FAA5BA',
+            green: '#34E60F',
+            yellow: '#E5D30F',
+            blue: '#0A29CC',
+            magenta: '#C3A3FA',
+            cyan: '#07CCD9',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FFB5BE',
+            lightGreen: '#3AFF11',
+            lightYellow: '#FFEB10',
+            lightBlue: '#0C33FF',
+            lightMagenta: '#CDBCFF',
+            lightCyan: '#08F0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#FA0101',
-      green: '#00651D',
-      yellow: '#FAD209',
-      blue: '#004AFF',
-      magenta: '#AE07E8',
-      cyan: '#006AEB',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#FA2A0F',
-      lightGreen: '#006D1D',
-      lightYellow: '#FAE935',
-      lightBlue: '#1A5CFF',
-      lightMagenta: '#AE47E8',
-      lightCyan: '#61DBEB',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'wasp',
-    side: 'heroes',
-    colors: {
-      backgroundColor: '#C6BB54',
-      tabs: '#817537',
-      activeTabBottomColor: '#42B9DF'
+    {
+        name: 'ms-marvel',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#A58A49',
+            tabs: '#2778AB',
+            activeTabBottomColor: '#A9443D'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#FA0101',
+            green: '#00651D',
+            yellow: '#FAD209',
+            blue: '#004AFF',
+            magenta: '#AE07E8',
+            cyan: '#006AEB',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FA2A0F',
+            lightGreen: '#006D1D',
+            lightYellow: '#FAE935',
+            lightBlue: '#1A5CFF',
+            lightMagenta: '#AE47E8',
+            lightCyan: '#61DBEB',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#E8110E',
-      green: '#139900',
-      yellow: '#FFF962',
-      blue: '#480DFF',
-      magenta: '#AE07E8',
-      cyan: '#0078E8',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#E83735',
-      lightGreen: '#588730',
-      lightYellow: '#FFFE84',
-      lightBlue: '#4657E8',
-      lightMagenta: '#C13FE8',
-      lightCyan: '#3193E8',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'wonder-woman',
-    side: 'heroes',
-    colors: {
-      backgroundColor: '#6B8D8D',
-      tabs: '#F5F21F',
-      activeTabBottomColor: '#ED0C05'
+    {
+        name: 'wasp',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#C6BB54',
+            tabs: '#817537',
+            activeTabBottomColor: '#42B9DF'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#E8110E',
+            green: '#139900',
+            yellow: '#FFF962',
+            blue: '#480DFF',
+            magenta: '#AE07E8',
+            cyan: '#0078E8',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#E83735',
+            lightGreen: '#588730',
+            lightYellow: '#FFFE84',
+            lightBlue: '#4657E8',
+            lightMagenta: '#C13FE8',
+            lightCyan: '#3193E8',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#ED0C05',
-      green: '#34E60F',
-      yellow: '#E5D30F',
-      blue: '#0A29CC',
-      magenta: '#C3A3FA',
-      cyan: '#07CCD9',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#FFB5BE',
-      lightGreen: '#3AFF11',
-      lightYellow: '#F5F21F',
-      lightBlue: '#0C33FF',
-      lightMagenta: '#CDBCFF',
-      lightCyan: '#08F0FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'x-23',
-    side: 'heroes',
-    colors: {
-      backgroundColor: '#7D929D',
-      tabs: '#38464E',
-      activeTabBottomColor: '#9B8463'
+    {
+        name: 'wonder-woman',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#6B8D8D',
+            tabs: '#F5F21F',
+            activeTabBottomColor: '#ED0C05'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#ED0C05',
+            green: '#34E60F',
+            yellow: '#E5D30F',
+            blue: '#0A29CC',
+            magenta: '#C3A3FA',
+            cyan: '#07CCD9',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FFB5BE',
+            lightGreen: '#3AFF11',
+            lightYellow: '#F5F21F',
+            lightBlue: '#0C33FF',
+            lightMagenta: '#CDBCFF',
+            lightCyan: '#08F0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#ED0C05',
-      green: '#34E60F',
-      yellow: '#E5D30F',
-      blue: '#0A29CC',
-      magenta: '#CD0DE8',
-      cyan: '#07CCD9',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#FF0D06',
-      lightGreen: '#3AFF11',
-      lightYellow: '#FFEB10',
-      lightBlue: '#0C33FF',
-      lightMagenta: '#E10EFF',
-      lightCyan: '#08F0FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'catwoman',
-    side: 'villians',
-    colors: {
-      backgroundColor: '#63768D',
-      tabs: '#554971',
-      activeTabBottomColor: '#B8F3FF'
+    {
+        name: 'x-23',
+        side: 'heroes',
+        colors: {
+            backgroundColor: '#7D929D',
+            tabs: '#38464E',
+            activeTabBottomColor: '#9B8463'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#ED0C05',
+            green: '#34E60F',
+            yellow: '#E5D30F',
+            blue: '#0A29CC',
+            magenta: '#CD0DE8',
+            cyan: '#07CCD9',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FF0D06',
+            lightGreen: '#3AFF11',
+            lightYellow: '#FFEB10',
+            lightBlue: '#0C33FF',
+            lightMagenta: '#E10EFF',
+            lightCyan: '#08F0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#CC0022',
-      green: '#00E025',
-      yellow: '#E5D30F',
-      blue: '#480DFF',
-      magenta: '#EB5AEA',
-      cyan: '#00eaff',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#FF7E8F',
-      lightGreen: '#CCFF00',
-      lightYellow: '#FFF962',
-      lightBlue: '#00eaff',
-      lightMagenta: '#F7B9FF',
-      lightCyan: '#B8F3FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'harley-quinn',
-    side: 'villians',
-    colors: {
-      backgroundColor: '#A7898C',
-      tabs: '#8C5050',
-      activeTabBottomColor: '#D1AF42'
+    {
+        name: 'catwoman',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#63768D',
+            tabs: '#554971',
+            activeTabBottomColor: '#B8F3FF'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#CC0022',
+            green: '#00E025',
+            yellow: '#E5D30F',
+            blue: '#480DFF',
+            magenta: '#EB5AEA',
+            cyan: '#00eaff',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#FF7E8F',
+            lightGreen: '#CCFF00',
+            lightYellow: '#FFF962',
+            lightBlue: '#00eaff',
+            lightMagenta: '#F7B9FF',
+            lightCyan: '#B8F3FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#FF032D',
-      green: '#00E025',
-      yellow: '#D1AF42',
-      blue: '#121156',
-      magenta: '#9E006E',
-      cyan: '#00758F',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#EB3994',
-      lightGreen: '#B2EDC1',
-      lightYellow: '#FFFC36',
-      lightBlue: '#1D2FD6',
-      lightMagenta: '#F7B9FF',
-      lightCyan: '#72E0FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'mystique',
-    side: 'villians',
-    colors: {
-      backgroundColor: '#A1ADA7',
-      tabs: '#125C9D',
-      activeTabBottomColor: '#6C221C'
+    {
+        name: 'harley-quinn',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#A7898C',
+            tabs: '#8C5050',
+            activeTabBottomColor: '#D1AF42'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#FF032D',
+            green: '#00E025',
+            yellow: '#D1AF42',
+            blue: '#121156',
+            magenta: '#9E006E',
+            cyan: '#00758F',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#EB3994',
+            lightGreen: '#B2EDC1',
+            lightYellow: '#FFFC36',
+            lightBlue: '#1D2FD6',
+            lightMagenta: '#F7B9FF',
+            lightCyan: '#72E0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#ED0C05',
-      green: '#19F710',
-      yellow: '#FFD400',
-      blue: '#0A29CC',
-      magenta: '#CD0DE8',
-      cyan: '#0974BD',
-      white: '#4C4C4C',
-      lightBlack: '#000000',
-      lightRed: '#FF0D06',
-      lightGreen: '#3AFF11',
-      lightYellow: '#FFD922',
-      lightBlue: '#0C33FF',
-      lightMagenta: '#E10EFF',
-      lightCyan: '#08F0FF',
-      lightWhite: '#FFFFFF'
-    }
-  },
-  {
-    name: 'phoenix',
-    side: 'villians',
-    colors: {
-      backgroundColor: '#F8D082',
-      tabs: '#7C3129',
-      activeTabBottomColor: '#ED9646'
+    {
+        name: 'mystique',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#A1ADA7',
+            tabs: '#125C9D',
+            activeTabBottomColor: '#6C221C'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#ED0C05',
+            green: '#19F710',
+            yellow: '#FFD400',
+            blue: '#0A29CC',
+            magenta: '#CD0DE8',
+            cyan: '#0974BD',
+            white: '#4C4C4C',
+            lightBlack: '#000000',
+            lightRed: '#FF0D06',
+            lightGreen: '#3AFF11',
+            lightYellow: '#FFD922',
+            lightBlue: '#0C33FF',
+            lightMagenta: '#E10EFF',
+            lightCyan: '#08F0FF',
+            lightWhite: '#FFFFFF'
+        }
+    }, {
+        name: 'star-sapphire',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#420e38',
+            tabs: '#c430af',
+            activeTabBottomColor: '#e0a6d8'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#e0174c',
+            green: '#17e068',
+            yellow: '#d6e017',
+            blue: '#1772e0',
+            magenta: '#9E006E',
+            cyan: '#17e0d1',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#f297f2',
+            lightGreen: '#B2EDC1',
+            lightYellow: '#d6e017',
+            lightBlue: '#1D2FD6',
+            lightMagenta: '#F7B9FF',
+            lightCyan: '#72E0FF',
+            lightWhite: '#FFFFFF'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#E8110E',
-      green: '#139900',
-      yellow: '#FF8805',
-      blue: '#480DFF',
-      magenta: '#AE07E8',
-      cyan: '#0078E8',
-      white: '#696969',
-      lightBlack: '#000000',
-      lightRed: '#E83735',
-      lightGreen: '#689F38',
-      lightYellow: '#E87F2A',
-      lightBlue: '#4657E8',
-      lightMagenta: '#C13FE8',
-      lightCyan: '#3193E8',
-      lightWhite: '#696969'
-    }
-  },
-  {
-    name: 'poison-ivy',
-    side: 'villians',
-    colors: {
-      backgroundColor: '#E29C9C',
-      tabs: '#B62A2F',
-      activeTabBottomColor: '#98C687'
+    {
+        name: 'phoenix',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#F8D082',
+            tabs: '#7C3129',
+            activeTabBottomColor: '#ED9646'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#E8110E',
+            green: '#139900',
+            yellow: '#FF8805',
+            blue: '#480DFF',
+            magenta: '#AE07E8',
+            cyan: '#0078E8',
+            white: '#696969',
+            lightBlack: '#000000',
+            lightRed: '#E83735',
+            lightGreen: '#689F38',
+            lightYellow: '#E87F2A',
+            lightBlue: '#4657E8',
+            lightMagenta: '#C13FE8',
+            lightCyan: '#3193E8',
+            lightWhite: '#696969'
+        }
     },
-    terminalTextColors: {
-      black: '#000000',
-      red: '#E8110E',
-      green: '#11792F',
-      yellow: '#FFE603',
-      blue: '#480DFF',
-      magenta: '#9E006E',
-      cyan: '#00888F',
-      white: '#FFFFFF',
-      lightBlack: '#000000',
-      lightRed: '#F03A30',
-      lightGreen: '#1F904D',
-      lightYellow: '#FFFC36',
-      lightBlue: '#4657E8',
-      lightMagenta: '#9E458B',
-      lightCyan: '#298AEB',
-      lightWhite: '#FFFFFF'
+    {
+        name: 'poison-ivy',
+        side: 'villians',
+        colors: {
+            backgroundColor: '#E29C9C',
+            tabs: '#B62A2F',
+            activeTabBottomColor: '#98C687'
+        },
+        terminalTextColors: {
+            black: '#000000',
+            red: '#E8110E',
+            green: '#11792F',
+            yellow: '#FFE603',
+            blue: '#480DFF',
+            magenta: '#9E006E',
+            cyan: '#00888F',
+            white: '#FFFFFF',
+            lightBlack: '#000000',
+            lightRed: '#F03A30',
+            lightGreen: '#1F904D',
+            lightYellow: '#FFFC36',
+            lightBlue: '#4657E8',
+            lightMagenta: '#9E458B',
+            lightCyan: '#298AEB',
+            lightWhite: '#FFFFFF'
+        }
     }
-  }
 ]
